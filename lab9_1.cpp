f7ce163f5ead5ff722e07bd922a0e2981c84994b
Ploythun <ploytwrsrijandon@gmail.com>
2025-12-19 16:02:59 +0700

    Lab9 complete

--- lab9_1.cpp
+++ lab9_1.cpp
@@ -3,9 +3,14 @@
 using namespace std;
 
 int main(){	
+	double loan, interest, payback, PrevBalance, Interest, Total, Payment, NewBalance;
+	int i;
 	cout << "Enter initial loan: ";
+	cin >> loan;
 	cout << "Enter interest rate per year (%): ";
+	cin >> interest;
 	cout << "Enter amount you can pay per year: ";
+	cin >> payback;
 
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
@@ -17,17 +22,33 @@ int main(){
 	cout << setw(13) << left << "Payment";
 	cout << setw(13) << left << "NewBalance";
 	cout << "\n";
+	PrevBalance = loan;
+	Payment = payback;
+	NewBalance = loan;
+	
+
+	for(i=1; NewBalance!=0; i+=1){
+	cout << fixed << setprecision(2); 
+	cout << setw(13) << left << i; 
+	cout << setw(13) << left << PrevBalance;
+	
+		Interest = PrevBalance*(interest/100);
+		Total = PrevBalance + Interest;
+		NewBalance = Total - Payment;
+		PrevBalance = NewBalance;
+
+	if(Total <= Payment){
+			Payment = Total;
+			NewBalance = 0;
+	}
 	
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
-	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
+	cout << setw(13) << left << Interest;
+	cout << setw(13) << left << Total;
+	cout << setw(13) << left << Payment;
+	cout << setw(13) << left << NewBalance;
 	cout << "\n";	
-	
+	}
 	return 0;
 }
--- lab9_2.cpp
+++ lab9_2.cpp
@@ -2,6 +2,17 @@
 using namespace std;
 
 //Write the function printO() here
+void printO(int N, int M){
+	if(N <= 0 || M <= 0){
+		cout << "Invalid input";
+	}
+	for(int i = 0; i<N; i++){
+		for(int j = 0; j<M; j++){
+			cout << "O";
+		}
+		cout << "\n";
+	}
+}
 
 int main(){
 	
@@ -15,7 +26,7 @@ int main(){
 	cout << "\n";	
 	
 	printO(0,3);
-	cout << "\n";	
+	cout << "\n";
 	
 	printO(7,-1);
 	cout << "\n";	
